2ab2c500aad50adacf23d8541d228d63e726599d
KING_WU <583775448@qq.com>
2018-12-14 11:32:25 +0800

    手续费策略

--- src/pages/settings/poundage/poundage.ts
+++ src/pages/settings/poundage/poundage.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { Storage } from '@ionic/storage';
 import { TranslateService } from '@ngx-translate/core';
 import { NavController } from 'ionic-angular';
 import { Logger } from '../../../providers/logger/logger';
@@ -18,20 +19,42 @@ import { ReplaceParametersProvider } from '../../../providers/replace-parameters
 export class PoundagePage {
   langs;
   langForm;
-
+  private time: number = 30;
+  private byte: number = 20;
   constructor(
     private navCtrl: NavController,
     private appProvider: AppProvider,
     private logger: Logger,
     private externalLinkProvider: ExternalLinkProvider,
     private replaceParametersProvider: ReplaceParametersProvider,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private storage: Storage
   ) {
-    this.langForm = new FormGroup({
-      langs: new FormControl({ value: 'conventional', disabled: false })
+    this.storage.get('poundage').then(val => {
+      this.langForm = new FormGroup({
+        langs: new FormControl({ value: val, disabled: false })
+      });
     });
   }
+  ionViewDidEnter() {}
   langSelect() {
     this.logger.info('langSelect: ' + this.langForm.value.langs);
+    if (this.langForm.value.langs == 'veryfast') {
+      this.time = 20;
+      this.byte = 77;
+    } else if (this.langForm.value.langs == 'givepriority') {
+      this.time = 20;
+      this.byte = 52;
+    } else if (this.langForm.value.langs == 'conventional') {
+      this.time = 30;
+      this.byte = 20;
+    } else if (this.langForm.value.langs == 'slower') {
+      this.time = 60;
+      this.byte = 14;
+    } else if (this.langForm.value.langs == 'slow') {
+      this.time = 240;
+      this.byte = 11;
+    }
+    this.storage.set('poundage', this.langForm.value.langs);
   }
 }
