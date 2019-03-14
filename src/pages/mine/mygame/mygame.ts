b957a99e4a76bce307492efa10c36a4e29a3e598
ThomasFuzhou <29013551@qq.com>
2019-03-14 17:10:03 +0800

    fix

--- src/pages/mine/mygame/mygame.ts
+++ src/pages/mine/mygame/mygame.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION, ViewChild } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Storage } from '@ionic/storage';
 import { TranslateService } from '@ngx-translate/core';
 import {
@@ -20,6 +21,8 @@ import { LoginPage } from '../login/login';
 export class MyGamePage {
   private authoriedGames: any;
   private index: number = 1;
+
+  private forumDiscuss: object; // 有用
   constructor(
     private navCtrl: NavController,
     private logger: Logger,
@@ -27,7 +30,8 @@ export class MyGamePage {
     private events: Events,
     private spvNodeProvider: SpvNodeProvider,
     private storage: Storage,
-    public navParams: NavParams
+    public navParams: NavParams,
+    private http: HttpClient,
   ) {
     this.storage.get('authorizedGame').then(val => {
       if (val == null) {
@@ -40,6 +44,12 @@ export class MyGamePage {
     });
   }
 
+  ionViewWillEnter() {
+    this.logger.info("进入我的游戏");
+    this.getForumDiscuss();
+
+  }
+
   openLoginPage() {
     this.navCtrl.push(LoginPage, {});
   }
@@ -48,4 +58,22 @@ export class MyGamePage {
   onSelect(index) {
     this.index = index;
   }
+
+  // 获取论坛中的评价信息数据（评价、点赞数）
+  getForumDiscuss() {
+    this.logger.info("获取论坛中的评价信息数据（评价、点赞数）");
+    let url = `http://121.40.82.216:8081/gamegoldWeb/port/discuss/page?game.gameName=cp0311`;
+    this.http.get(url).subscribe(
+      response => {
+        if (response == null) {
+          return;
+        }
+        this.logger.info("Get forumDiscussInfo: " + response);
+        this.forumDiscuss = response['list']; // 跳到集合这一层
+      },
+      error => {
+        this.logger.error("Get forumGameInfo: " + JSON.stringify(error));
+      });
+  }
+
 }
